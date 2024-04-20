7e4edea2823169c9277bc7c9bf2f583588b5dae7
Fernando Vásquez <f.vasquez@softec.cloud>
2024-04-21 00:12:53 +0200

    Adding images const

---
+++ src/consts/images.ts
@@ -0,0 +1,12 @@
+interface Image {
+    name: string;
+    src: string;
+}
+
+const IMAGES: Image[] = [
+    { name: 'Beach - 488 KB - 1920x1080', src: '/img/beach.jpg' },
+    { name: 'Map - 15.3 MB - 9489x7444', src: '/img/map.jpg' },
+    { name: 'Space - 19.1 MB - 6200x6200', src: '/img/space.jpg' },
+];
+
+export default IMAGES;
