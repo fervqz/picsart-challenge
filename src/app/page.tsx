adb93ec4190482b2163301347dfe233f57595132
Fernando Vásquez <f.vasquez@softec.cloud>
2024-04-20 14:03:18 +0200

    First Commit - Preview done, performance problems fixes in progress.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,92 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import React, { useEffect, useRef, useState } from "react";
+import ColorDropperPreview from "@/components/ColorDropperPreview.tas/ColorDropperPreview";
 
 export default function Home() {
+
+  const previewSize = 11;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const previewWrapperRef = useRef<HTMLDivElement>(null);
+  const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+  const [previewData, setPreviewData] = useState<Uint8ClampedArray>(new Uint8ClampedArray(20 * 20));
+
+  const imgRef = useRef<HTMLImageElement>(new Image());
+
+  useEffect(() => {
+
+    loadImg();
+
+    canvasRef.current?.setAttribute('width', '900px');
+    canvasRef.current?.setAttribute('height', '600px');
+
+    const ctx = canvasRef.current?.getContext("2d") ?? null;
+    setCtx(ctx);
+
+  }, []);
+
+  const loadImg = () => {
+    imgRef.current.src = "https://i.pinimg.com/564x/90/99/a7/9099a72884a5cc1c46ab1296546eac21.jpg";
+    imgRef.current.setAttribute('crossOrigin', 'anonymous');
+    imgRef.current.onload = () => {
+      ctx?.drawImage(imgRef.current, 0, 0, canvasRef.current?.width ?? 0, canvasRef.current?.height ?? 0);
+    };
+  }
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
+
+    ctx?.clearRect(0, 0, canvasRef.current?.width ?? 0, canvasRef.current?.height ?? 0);
+    ctx?.drawImage(imgRef.current, 0, 0, canvasRef.current?.width ?? 0, canvasRef.current?.height ?? 0);
+
+    const rect = canvasRef.current?.getBoundingClientRect() ?? { left: 0, top: 0 };
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const previewX = x - previewSize / 2;
+    const previewY = y - previewSize / 2;
+    const previewWidth = previewSize;
+    const previewHeight = previewSize;
+
+    previewWrapperRef.current.style.left = `${x}px`;
+    previewWrapperRef.current.style.top = `${y}px`;
+
+    // @ts-ignore
+    ctx.beginPath();
+    // @ts-ignore
+    ctx.lineWidth = "3";
+    // @ts-ignore
+    ctx.strokeStyle = "red";
+    // @ts-ignore
+    ctx.rect(previewX - 2, previewY - 2, previewWidth + 4, previewHeight + 4);
+    // @ts-ignore
+    ctx.stroke();
+
+    setPreviewData(ctx?.getImageData(previewX, previewY, previewWidth, previewHeight).data as Uint8ClampedArray);
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div>
+        <h1>Hellow World</h1>
+        <canvas
+          ref={canvasRef}
+          style={styles.mainCanvas}
+          onMouseMove={handleMouseMove}
+        ></canvas>
+        <div ref={previewWrapperRef} style={{ position: 'absolute' }}>
+          <ColorDropperPreview data={previewData} size={previewSize} />
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
+}
+
+const styles = {
+  mainCanvas: {
+    width: '900px',
+    height: '600px',
+    border: '1px solid black',
+    // display: 'block',
+    // marginInline: 'auto',
+  }
 }
