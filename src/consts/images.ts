17f1ed68d92435a2af3c5c0eb26dc85f98e0718c
Fernando Vásquez <f.vasquez@softec.cloud>
2024-04-22 00:35:57 +0200

    Add new images to IMAGES constant

--- src/consts/images.ts
+++ src/consts/images.ts
@@ -8,6 +8,16 @@ const IMAGES: Image[] = [
     { name: 'Map - 15.3 MB - 9489x7444', src: '/img/map.jpg' },
     { name: 'Space - 19.1 MB - 6200x6200', src: '/img/space.jpg' },
     { name: 'Landscape - 35.9 MB - 5184x3456', src: '/img/landscape.png' },
+    { name: 'Test Image 1 - 2.3 MB - 3200x3200', src: '/img/testing/img-test-1.jpg' },
+    { name: 'Test Image 2 - 262 KB - 3200x3200', src: '/img/testing/img-test-2.jpg' },
+    { name: 'Test Image 3 - 1.8 MB - 3200x3200', src: '/img/testing/img-test-3.jpg' },
+    { name: 'Test Image 4 - 1.7 MB - 3200x3200', src: '/img/testing/img-test-4.jpg' },
+    { name: 'Test Image 5 - 1.6 MB - 3200x3200', src: '/img/testing/img-test-5.jpg' },
+    { name: 'Test Image 6 - 1.7 MB - 3200x3200', src: '/img/testing/img-test-6.jpg' },
+    { name: 'Test Image 7 - 1.2 MB - 3200x3200', src: '/img/testing/img-test-7.jpg' },
+    { name: 'Test Image 8 - 5.3 MB - 3200x3200', src: '/img/testing/img-test-8.jpg' },
+    { name: 'Test Image 9 - 580 KB - 567x567', src: '/img/testing/img-test-9.png' },
+    { name: 'Test Image 10 - 2.48 KB - 300x3000', src: '/img/testing/img-test-10.png' },
 ];
 
 export default IMAGES;
